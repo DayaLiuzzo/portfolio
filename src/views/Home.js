a0857e658f5c31b87137a229e6877b4b39dce28c
Daya Liuzzo <daya.liuzzo@gmail.com>
2025-05-13 18:21:50 +0200

    pre deployment

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,5 @@
 
 import BaseView from "./BaseView.js";
-import javascriptLogo from './../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './../counter.js'
 let isRunning = true;
 
 
